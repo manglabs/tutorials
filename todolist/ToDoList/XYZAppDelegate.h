3eaee86755233c4a498f6539ee57d04e3ce5776a
Tim <manglabs@gmail.com>
2013-12-19 23:40:37 -0800

    todolist - this was a post-podification checkpoint

---
+++ todolist/ToDoList/XYZAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by timothy mansfield on 12/17/13.
+//  Copyright (c) 2013 mang labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AFNetworking/AFNetworking.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ todolist/ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by timothy mansfield on 12/17/13.
+//  Copyright (c) 2013 mang labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
